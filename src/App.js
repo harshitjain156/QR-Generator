703420b3d974a5a828b1f40f926076692c8b98c3
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-07-17 13:34:58 +0530

    Code base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Qrcode from './components/Qr';
+
+function App() {
+  return (
+    <div className="App">
+        <Qrcode />
+    </div>
+  );
+}
+
+export default App;
